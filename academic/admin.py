8e190c9808b4b84536586b611bdc75f562c04913
Makarand Bauskar <mbauskar@gmail.com>
2018-10-05 18:30:24 +0530

    [app] added academics app to manage student attendance, assessment

---
+++ academic/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import apps
+from django.contrib import admin
+
+# Register your models here.
+
+app = apps.get_app_config('academic')
+
+for model_name, model in app.models.items():
+    admin.site.register(model)
--- profiles/urls/student_urls.py
+++ profiles/urls/student_urls.py
@@ -4,11 +4,11 @@ from profiles.views import student_views
 urlpatterns = [
     url(r'^$', student_views.get_student_list, name="student-list"),
     url(r'^list$', student_views.get_student_list, name="student-list"),
-	url(r'^new/$', student_views.save_update_student, name="new-student"),
-	url(r'^(?P<prn>[\d]+)/$', student_views.get_student_profile,
+    url(r'^new/$', student_views.save_update_student, name="new-student"),
+    url(r'^(?P<prn>[\d]+)/$', student_views.get_student_profile,
         name="student-profile"),
-	url(r'^(?P<prn>[\d]+)/edit$', student_views.save_update_student,
+    url(r'^(?P<prn>[\d]+)/edit$', student_views.save_update_student,
         name="update-student"),
-	url(r'^(?P<prn>[\d]+)/delete$', student_views.delete_student,
+    url(r'^(?P<prn>[\d]+)/delete$', student_views.delete_student,
         name="delete-student"),
 ]
